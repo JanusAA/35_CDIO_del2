477c614573b2c2da29ce592b3ad899818d1a0525
JanusAA <janus.andreasson@hotmail.com>
2016-10-31 15:51:27 +0100

    Added UserLanguage array.

---
+++ 35_del2/src/del2/UserLanguage.java
@@ -0,0 +1,43 @@
+package del2;
+
+public class UserLanguage {
+
+/**	String sf1 = ("Du er landet på Tower felet, +250");
+//	String sf2 = ("Du er landet på Crater feltet, -100");
+//	String sf3 = ("Du er landet på Palace gates feltet, +100");
+//	String sf4 = ("Du er landet på Cold Desert feltet, -20");
+//	String sf5 = ("Du er landet på Walled city feltet, +180");
+//	String sf6 = ("Du er landet på Monastery, +0");
+//	String sf7 = ("Du er landet på Black cave feltet, -70");
+//	String sf8 = ("Du er landet på Huts in the mountaion feltet, +60");
+//	String sf9 = ("Du er landet på The Werewall feltet, -80 men du får et ekstra slag");
+//	String sf10 = ("Du er landet på The pit feltet, -50");
+//	String sf11 = ("du er landet på Goldmine feltet, +650");
+//	
+**/	
+	
+	//Vi laver et arrey af string objekter: 
+	static String[] felt = {
+			"Du er landet på Tower felet, +250",
+			"Du er landet på Crater feltet, -100",
+			"Du er landet på Palace gates feltet, +100",
+			"Du er landet på Cold Desert feltet, -20",
+			"Du er landet på Walled city feltet, +180",
+			"Du er landet på Monastery, +0",
+			"Du er landet på Black cave feltet, -70",
+			"Du er landet på Huts in the mountaion feltet, +60",
+			"Du er landet på The Werewall feltet, -80 men du får et ekstra slag",
+			"Du er landet på The pit feltet, -50",
+			"Du er landet på Goldmine feltet, +650"
+			};
+
+	
+
+	String spp = ("s point: ");
+	String swin = (" vandt spillet!");
+	String sstale = ("Spillet endte uafgjort");
+	
+	
+	
+	
+}
