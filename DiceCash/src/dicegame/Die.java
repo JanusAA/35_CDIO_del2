c31ee3627c0bf37a76f3a3b047e085cb9530cb91
Justin <jufa2401@gmail.com>
2016-10-31 16:04:20 +0100

    implementering af this.sides

--- DiceCash/src/dicegame/Die.java
+++ DiceCash/src/dicegame/Die.java
@@ -1,12 +1,12 @@
 package dicegame;
 
 public class Die {
-	 int faceValue, sides = 6;
-	 
-	 	
+	 int faceValue, sides; 
+	 	 	
 	    public void roll(){
 	        faceValue = (int)(Math.random() * sides + 1);
-	    }
+	        this.sides = 6;
+	   }
 
 	    public int getValue(){
 	        return faceValue;   
